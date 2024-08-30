5f0f57d0b1f86f2a75989f796df6510ffa2108d3
luanabrizola <130062674+luanabrizola@users.noreply.github.com>
2024-08-30 17:35:03 -0300

    tentado

--- landingpagenext/src/app/componentes/Form/Form.tsx
+++ landingpagenext/src/app/componentes/Form/Form.tsx
@@ -1,10 +1,68 @@
+"use client";
+
 import styles from "./Form.module.css"
+import React, { useState } from "react";
 
-export default function Form(){
+export default function Form() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [companyType, setCompanyType] = useState('');
+    const [businessSector, setBusinessSector] = useState('');
+    const [employees, setEmployees] = useState('');
 
-    return(
-        <div className={styles.Footer}>
-            <h1>Form</h1>
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log({ name, email, companyType, businessSector, employees });
+    };
+
+
+    return (
+        <div className={styles.Form}>
+            <div className={styles.formText}>
+                <h2>Receba a planilha por e-mail</h2>
+                <p>preencha seus dados para receber o material</p>
+            </div>
+            <form className={styles.formulario} onSubmit={handleSubmit}>
+                <label htmlFor="nome">Nome</label>
+                <input
+                    type="text"
+                    placeholder="Digite seu nome"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                />
+                <label htmlFor="">Email</label>
+                <input
+                    type="email"
+                    placeholder="Digite seu e-mail"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+                <label htmlFor="">Você possui empresa?</label>
+                <input
+                    type="text"
+                    value={companyType}
+                    onChange={(e) => setCompanyType(e.target.value)}
+                    required
+                />
+                <label htmlFor="">Qual o setor da sua empresa?</label>
+                <input
+                    type="text"
+                    value={businessSector}
+                    onChange={(e) => setBusinessSector(e.target.value)}
+                    required
+                />
+                <label htmlFor="">Quantas pessoas tem na sua empresa?</label>
+                <input
+                    type="number"
+                    value={employees}
+                    onChange={(e) => setEmployees(e.target.value)}
+                    required
+                />
+                <button className={styles.botao}>Receber o material</button>
+            </form>
+
         </div>
     )
 }
--- landingpagenext/src/app/page.tsx
+++ landingpagenext/src/app/page.tsx
@@ -2,13 +2,20 @@ import Header from "./componentes/Header/Header";
 import Main from "./componentes/Main/Main";
 import Form from "./componentes/Form/Form";
 import Info from "./componentes/Info/Info";
+import styles from "./page.module.css"
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <main>
-        <Main />
+      <main className={styles.mainContainer}>
+        <div className={styles.mainContent}>
+          <Main />
+        </div>
+        <div className={styles.formEInfoContent}>
+          <Form />
+        </div>
+        
       </main>
     </div>
   );
